2c2656fffe27386b6d680bb05136d3ec81693d1a
Nick Tse <nicholashtse@gmail.com>
2022-06-04 18:28:58 -0700

    Add UI logic to display conversion result

--- src/index.js
+++ src/index.js
@@ -12,5 +12,14 @@ function clearFields() {
 }
 
 $(document).ready(function() {
-
+  $('#usdConvert').click(function() {
+    let currencyToConvert = $('#currency').val();
+    clearFields();
+    let promise = CurrencyExchanger.getEquivalent(currencyToConvert);
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $('.showEquivalent').text(`The equivalent of ${usdValue} USD in ${currency} is ${body.conversion_result}`);
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+    });
+  });
 }); 
