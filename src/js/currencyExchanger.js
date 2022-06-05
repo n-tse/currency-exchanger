c0bf88240af6318a6c14cc30c9b54de11ad7521c
Nick Tse <nicholashtse@gmail.com>
2022-06-05 08:22:30 -0700

    Attempt to debug and see why things aren't wired correctly

--- src/js/currencyExchanger.js
+++ src/js/currencyExchanger.js
@@ -2,6 +2,8 @@ export default class CurrencyExchanger {
   static getEquivalent(currency) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
+      console.log(${currency});
+      console.log(${usdValue});
       const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}/${usdValue}`;
       request.onload = function() {
         if (this.status === 200) {
