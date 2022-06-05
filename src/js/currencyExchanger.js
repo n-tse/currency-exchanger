45de60748981d8f2bed48ab5faa69b334c82addb
Nick Tse <nicholashtse@gmail.com>
2022-06-04 17:24:09 -0700

    Update API logic file to make get request and handle response

--- src/js/currencyExchanger.js
+++ src/js/currencyExchanger.js
@@ -1,1 +1,17 @@
-//for business logic, constructors, and functions
+export default class CurrencyExchanger {  
+  static getEquivalent(currency) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
