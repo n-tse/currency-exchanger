876a8a6f171ec841a45d6b0717fbaab0869b4252
Nick Tse <nicholashtse@gmail.com>
2022-06-04 17:49:06 -0700

    Update Pair endpoint to also take amount to be converted

--- src/js/currencyExchanger.js
+++ src/js/currencyExchanger.js
@@ -2,7 +2,7 @@ export default class CurrencyExchanger {
   static getEquivalent(currency) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}`;
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}/${usdValue}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
